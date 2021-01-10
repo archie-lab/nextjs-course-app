cdd46706a42278700a9619e7011abb0c534ca04a
Artyom Shymukovich <artyom.shymukovich@nindatech.com>
2021-01-10 13:41:48 +0800

    added simple theme

---
+++ pages/_app.jsx
@@ -0,0 +1,11 @@
+import { jsx } from "theme-ui";
+import { ThemeProvider } from "theme-ui";
+import theme from "../theme";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Link from "next/link";
 
 const Page = () => (
-    <div>
-         <h1>Index page</h1>
-         <Link  ef="/notes"><a>Notes</a></Link>
-    </div>
+  <div>
+    <h1>Index page</h1>
+    <Link href="/notes">
+      <a>Notes</a>
+    </Link>
+  </div>
 );
 
 export default Page;
